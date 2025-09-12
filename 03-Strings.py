8687c74a039ba1e4f1bfc088169c6039e8ec6e8b
Hassan Mahmood <hma.hassan.2001@gmail.com>
2025-09-12 12:22:02 +0500

    strings Revision Done

--- 03-Strings.py
+++ 03-Strings.py
@@ -1,4 +1,4 @@
-var = "Hello world!"
+var = "Hello world"
 multiline_str = """
 This is multi-line String.
 """
@@ -11,28 +11,29 @@ var[::]   # Copy the whole string
 var[::-1] # Reverse the string
 var[0:7:2] # From index 0 to 6, step 2
 
-# String Methods
-len(var)         # Length of the string
+# String Methods (Not in-place)
+len(var)        
 
 var.lower()      # to lowercase
 var.upper()      # uppercase
 var.capitalize()  # First letter uppercase
-var.title()      # First letter of each word uppercase
+var.title()      # First letter of each word uppercase 
 
-var.strip()      # Remove whitespace
-var.replace("world", "Python") # Replace substring not in-place
+var.strip()      # Remove whitespace 
+var.replace("world", "Python") # Replace substring
 var.split(" ")  # Split string into list by delimiter
 var.join(["Hello", "Python"]) # Join list of strings with var as separator
+print(var.join(["Hello ", " Python"]))
 
-var.index("o")  # Find index of first occurrence of substring
-var.index("X") # returns Value Error.
+var.index("o")  # Find index of first occurrence.
+# var.index("X")  --> returns Value Error.
 var.find("X")  # same as Index, but returns -1 if not found
 
-var.count("l")  # Count of occurrences
-var.isalpha()   # Check if all characters are alphabetic
-var.isdigit()   # Check if all characters are digits
-var.isalnum()   # Check if all characters are alphanumeric
-var.isspace()   # Check if all characters are whitespace
+print( var.count("l"))   # Count of occurrences
+print( var.isalpha() )   # Check if all characters are alphabetic (no special chars or spaces)
+print( var.isdigit() )   # Check if all characters are digits
+print( var.isalnum() )   # Check if all characters are alphanumeric (no special chars or spaces, but can have digits)
+print( var.isspace() )   # Check if all characters are whitespace
 
 
 
